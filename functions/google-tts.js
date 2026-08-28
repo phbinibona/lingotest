e31c10a3b5ed720e73b64dac706ec971d58cd5b9
phbinibona <phbinibona@gmail.com>
2026-08-29 00:58:11 +0200

    Fix grant_type in Google TTS token request

--- functions/google-tts.js
+++ functions/google-tts.js
@@ -71,7 +71,7 @@ async function serviceAccountToken(){
     method:"POST",
     headers:{"Content-Type":"application/x-www-form-urlencoded"},
     body:new URLSearchParams({
-      grant_type:"urn:ietf:params:oauth2:grant-type:jwt-bearer",
+      grant_type:"urn:ietf:params:oauth:grant-type:jwt-bearer",
       assertion:jwt
     })
   });
